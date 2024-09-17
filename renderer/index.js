71158d77d0a5fcd5985a1b756d87f039ea6cfb2f
Agustín Stringa <stringaagustin1@gmail.com>
2024-09-17 11:06:52 -0300

    feat: initial CREATE expense

--- renderer/index.js
+++ renderer/index.js
@@ -3,3 +3,25 @@
 document.getElementById("categoriesBtn").addEventListener("click", () => {
   window.api.send("categories-window");
 });
+
+document.getElementById("addExpenseBtn").addEventListener("click", () => {
+  window.api.send("add-expense-window");
+});
+
+window.api.on("update-expenses", (expenses) => {
+  const ul = document.getElementById("expensesList");
+  if (expenses.length > 0) {
+    const items = expenses.reduce((html, e) => {
+      html += `<div>
+      <p>id: ${e.id} </p>
+      <p>nombre: ${e.name} </p>
+      <p>cantidad: ${e.amount} </p>
+      <p>categoria: ${e.category.name} </p>
+      </div>`;
+      return html;
+    }, "");
+    ul.innerHTML = items;
+  } else {
+    ul.innerHTML = `<p>No hay gastos registrados aún. Agrega uno para comenzar</p>`;
+  }
+});
